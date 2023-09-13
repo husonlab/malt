10e91dacb165eef895d775d4503ff59922fef5b8
Daniel <daniel.huson@uni-tuebingen.de>
2023-09-13 14:56:56 +0800

    new release

--- src/malt/Version.java
+++ src/malt/Version.java
@@ -24,5 +24,5 @@ package malt;
  */
 public class Version {
     public static final String NAME = "MALT";
-	public static final String SHORT_DESCRIPTION = "MALT (version 0.6.1, built 25 Oct 2022)";
+	public static final String SHORT_DESCRIPTION = "MALT (version 0.6.2, built 12 Sep 2023)";
 }
